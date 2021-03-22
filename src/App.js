9c800eb6378c4d5a322aec257d01a7b6e69cd384
joesid <joesidious@gmail.com>
2021-03-22 02:58:52 +0100

    initalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar.js';
+import ProductList from './components/ProductList.js';
+import Details from './components/Details.js'
+import Cart from './components/Cart.js';
+import Default from './components/Default.js';
+// import Product from './components/Product.js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <ProductList />
+    <Details />
+    <Cart />
+    <Default />
+     
+    </>
   );
 }
 
