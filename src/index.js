8d3eb8a91cf504c334b7dd2dbb98a63318e0bfdc
joesid <joesidious@gmail.com>
2021-03-24 11:07:33 +0100

    Modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {Switch, Route,BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
