f7032323ed8bd43eb4aba79a20f3f2e6231b4c0f
joesid <joesidious@gmail.com>
2021-03-24 07:43:06 +0100

    Add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {Switch,Route,BrowserRouter as Router} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,15 +11,17 @@ import Default from './components/Default.js';
 // import Product from './components/Product.js';
 
 
-
 function App() {
   return (
     <>
     <Navbar />
-    <ProductList />
-    <Details />
-    <Cart />
-    <Default />
+    <Switch>
+       <Route exact path="/ProductList" component={ProductList}/>
+      <Route path="/details" component={Details}/>
+      <Route path="/cart" component={Cart}/>
+      <Route component ={Default}/>
+    </Switch>
+   
      
     </>
   );
