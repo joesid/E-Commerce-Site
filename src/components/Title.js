235bb82865a70d67e114af86923e574adf9ec29c
joesid <joesidious@gmail.com>
2021-03-26 09:47:57 +0100

    Title.js modified

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,6 +5,9 @@ export default function Title(name, title) {
         <div className = "row">
         <div className = "col-10 mx-auto my-2 text-center text-title">
             </div>   
+            <h1 className="text-capitalize font-weight-bold">{name}
+            <strong className = "text-blue">{title}</strong>
+            </h1>
         </div>
     )
 }
