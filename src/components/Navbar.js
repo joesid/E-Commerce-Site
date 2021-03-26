81ba9fbcd5f1143acda371b35c28e7210d3467ce
joesid <joesidious@gmail.com>
2021-03-26 07:59:17 +0100

    initialized Title.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React, { Component} from "react";
 import {Link} from "react-router-dom";
 import logo from '../logo.svg';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import {ButtonContainer} from './Button';
+import cart_logo from '../icons/cart_logo.svg';
+
 
 export default class Navbar extends Component {
     render(){
         return (
             <div>
              <h3>NavBar </h3>
-             <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+             <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/*https://www.iconfinder.com/icons1243689/call_phone_icon Creative Commones (Attribution 3.0 Unported);
                  https://www.iconfinder.com/Makoto_msk */}
                  <Link to = '/'>
@@ -17,7 +19,7 @@ export default class Navbar extends Component {
                  </Link>
                  <ul className="navbar-nav align-items-center">
                    <li className = "nav=item ml-5" >
-                      <Link to="/" className="nav-link">
+                      <Link to="/ProductList" className="nav-link">
                          products
                       </Link>
                    </li>   
@@ -25,14 +27,40 @@ export default class Navbar extends Component {
                 <Link to='/cart' className="ml-auto">
                 <ButtonContainer>
                     <span className="mr-2">
+                     <CartLogo />
                    <i className="fas fa-cart-plus"/>
                    </span>
                    My Cart
                 </ButtonContainer>
                 </Link>
-             </nav>
+             </NavWrapper>
             </div>
         )
     }
 }
 
+
+
+
+   // --- Trying to do a conditional rendering of the cart icon in the event 
+   //--- the one from an external site doesn' render
+function CartLogo (){
+   return <img src={cart_logo} height={20
+   } />
+}
+   
+function CartCss(){
+   return <i className="fas fa-cart-plus"/>
+}
+   
+// ------------------
+
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+   color:var(--mainWhite)!important;
+   font-size:1.3rem;
+   text-transform:capitalize;
+
+}
+`; 
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title(name, title) {
+    return (
+        <div className = "row">
+        <div className = "col-10 mx-auto my-2 text-center text-title">
+            </div>   
+        </div>
+    )
+}
