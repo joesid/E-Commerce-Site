e03a01e4e064e25a4a37bc96fe5c35609810144a
joesid <joesidious@gmail.com>
2021-03-26 18:52:59 +0100

    Modified

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import Product from './Product.js'
+import Title from './Title';
+import {storeProducts} from '../data.js';
+import {ProductConsumer} from '../context';
+
 
 export default class ProductList extends Component{
+    state = {
+        products: storeProducts
+    }
     render(){
+        console.log(this.state.products)
         return(
-            <div>
-                <Product />
-            </div>
+        <React.Fragment>
+          <div className = "py-5">
+            <div className = "container">
+              <Title name="our" title="products" />
+
+              <div className = "row">  
+              <ProductConsumer>
+               {hello =>{
+                return <h1>{hello}</h1>;
+               }}
+              </ProductConsumer>
+          </div>
+         </div>
+        </div>          
+     </React.Fragment>
+            // <Product /> 
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import {Switch, Route,BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ProductProvider} from './context';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <ProductProvider>
     <Router>
     <App />
     </Router>
+    </ProductProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
